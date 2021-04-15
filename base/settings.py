6ab9162d5e681d4bb1af5db98c2475c5b2082813
animesharma3 <animesharma3@gmail.com>
2021-04-15 19:14:30 +0530

    Deploy

--- base/settings.py
+++ base/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l)jjnuexzt&jgdbn&ln#+5ro*ys1ar!reknd_l#q(!@ar8lnva'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
